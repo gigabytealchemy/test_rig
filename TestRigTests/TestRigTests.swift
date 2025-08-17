f5a665d02298a039552b3c5d7dda15087f3403ce
Martin Westhead <martin-github@westhead.me>
2025-08-17 15:45:50 -0700

    Initial commit

---
+++ TestRigTests/TestRigTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestRigTests.swift
+//  TestRigTests
+//
+//  Created by Martin Westhead on 8/17/25.
+//
+
+import Testing
+@testable import TestRig
+
+struct TestRigTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
